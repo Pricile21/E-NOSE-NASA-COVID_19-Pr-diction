1b3bb981d730e62fb6730a10e96f3924de9ef7d1
GANGBE Pricile <151623722+Pricile21@users.noreply.github.com>
2024-08-17 00:11:29 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, File, UploadFile
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import requests
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split 
 from tensorflow.keras.models import load_model
