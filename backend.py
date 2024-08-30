f7525aac17bfd9599b44d3e31306dcf8ac5c57ef
GANGBE Pricile <151623722+Pricile21@users.noreply.github.com>
2024-08-30 19:30:27 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,15 +5,12 @@ import numpy as np
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model, save_model
 
 app = FastAPI()
 
-# Chemin du modèle
-MODEL_PATH = "https://github.com/Pricile21/E-NOSE-NASA-COVID_19-Pr-diction/raw/master/best_model.keras"
-
-# Charger le modèle
-model = load_model(MODEL_PATH)
+# Chemin du modèle local
+MODEL_PATH = "best_model.keras"
 
 # Scaler global, avec un attribut pour savoir s'il est ajusté
 scaler = StandardScaler()
@@ -35,7 +32,7 @@ def preprocess_data(data, is_training=True):
         X = data
         return X
 
-TRAIN_DATA_PATH = "https://github.com/Pricile21/E-NOSE-NASA-COVID_19-Pr-diction/raw/master/train_data.csv"
+TRAIN_DATA_PATH = "train_data.csv"
 
 @app.get("/")
 async def root():
@@ -57,10 +54,11 @@ async def train_model(file: UploadFile = File(...)):
         with open(file_location, "wb") as f:
             f.write(file.file.read())
         train_data = pd.read_csv(file_location)
-        print(train_data.head())
         X_train, y_train = preprocess_data(train_data, is_training=True)
         X_train_scaled = scaler.transform(X_train) if scaler_fitted else scaler.fit_transform(X_train)
         X_train_split, X_val_split, y_train_split, y_val_split = train_test_split(X_train_scaled, y_train, test_size=0.2, random_state=42)
+        
+        # Définir et entraîner le modèle
         model = tf.keras.models.Sequential([
             tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train_split.shape[1],)),
             tf.keras.layers.Dense(32, activation='relu'),
@@ -68,10 +66,15 @@ async def train_model(file: UploadFile = File(...)):
             tf.keras.layers.Dense(1, activation='sigmoid')
         ])
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        
+        # Callbacks pour sauvegarde et early stopping
         model_ckp = tf.keras.callbacks.ModelCheckpoint(filepath=MODEL_PATH, monitor="val_accuracy", mode="max", save_best_only=True)
         stop = tf.keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=200, restore_best_weights=True)
+        
+        # Entraînement du modèle
         model.fit(X_train_split, y_train_split, epochs=200, batch_size=2048, validation_data=(X_val_split, y_val_split), callbacks=[model_ckp, stop])
         model.save(MODEL_PATH)
+        
         return {"message": "Modèle entraîné avec succès."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -97,3 +100,4 @@ async def make_prediction(file: UploadFile = File(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
