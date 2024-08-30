7499f1f897afb0cc65860b6d8acc6f146d98024f
GANGBE Pricile <151623722+Pricile21@users.noreply.github.com>
2024-08-30 19:43:46 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -97,7 +97,4 @@ async def make_prediction(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
